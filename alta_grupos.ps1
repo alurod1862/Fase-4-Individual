59352326e182c05cfed261298dfcd53c7a9d5dbe
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2021-11-18 23:05:35 +0100

    Update alta_grupos.ps1

--- alta_grupos.ps1
+++ alta_grupos.ps1
@@ -1,1 +1,6 @@
-a
+$gruposCsv=Read-Host "Introduce el fichero csv de Grupos"
+$fichero=import-csv -Path $gruposCsv -delimiter :
+foreach($line in $fichero)
+{
+    New-ADGroup -Name:$line.Name -Path:$line.Path -Description:$line.Description -GroupCategory:$line.Category -GroupScope:$line.Scope
+}

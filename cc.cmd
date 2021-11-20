f3a7451c0d17c7f54c3f1a91e592822bd7b9b5f2
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2021-11-20 16:25:31 +0100

    Update cc.cmd

--- cc.cmd
+++ cc.cmd
@@ -1,33 +1,33 @@
 @echo off
-REM
+REM //Creamos las carpetas
 if NOT EXIST E:\Dep-Gandia mkdir E:\Dep-Gandia
 if NOT EXIST E:\Dep-Gandia\RRHH mkdir E:\Dep-Gandia\RRHH
 if NOT EXIST E:\Dep-Gandia\Prensa mkdir E:\Dep-Gandia\Prensa
 if NOT EXIST E:\Dep-Gandia\Informatica mkdir E:\Dep-Gandia\Informatica
 if NOT EXIST E:\Dep-Gandia\Investigacion_desarollo mkdir E:\Dep-Gandia\Investigacion_desarollo
-REM
+REM // Las compartimos por red
 net share Dep-RRHH=E:\Dep-Gandia\RRHH /GRANT:Gandia_GL_WR-RRHH,change /GRANT:Gandia_GL_Lectura,read
 net share Dep-Prensa=E:\Dep-Gandia\Prensa /GRANT:Gandia_GL_WR-Prensa,change /GRANT:Gandia_GL_Lectura,read
 net share Dep-Informatica=E:\Dep-Gandia\Informatica /GRANT:Gandia_GL_WR-Informatica,change /GRANT:Gandia_GL_Lectura,read
 net share Dep-Investigacion_desarollo=E:\Dep-Gandia\Investigacion_desarollo /GRANT:Gandia_GL_WR-Investigacion_desarollo,change /GRANT:Gandia_GL_Lectura,read
-REM
+REM //añandimos los grupos y permisos de seguridad
 icacls E:\Dep-Gandia\RRHH /GRANT gandia\Gandia_GL_WR-RRHH:(R,W)
 icacls E:\Dep-Gandia\Prensa /GRANT gandia\Gandia_GL_WR-Prensa:(R,W)
 icacls E:\Dep-Gandia\Informatica /GRANT gandia\Gandia_GL_WR-Informatica:(R,W)
 icacls E:\Dep-Gandia\Investigacion_desarollo /GRANT gandia\Gandia_GL_WR-Investigacion_desarollo:(R,W)
-REM
+REM  //Quitamos la herencia
 icacls E:\Dep-Gandia /inheritance:d
 icacls E:\Dep-Gandia\RRHH /inheritance:d
 icacls E:\Dep-Gandia\Prensa /inheritance:d
 icacls E:\Dep-Gandia\Informatica /inheritance:d
 icacls E:\Dep-Gandia\Investigacion_desarollo /inheritance:d
-REM
+REM  //Quitamos el grupo Usuarios
 icacls E:\Dep-Gandia  /remove:g Usuarios
 icacls E:\Dep-Gandia\RRHH  /remove:g Usuarios
 icacls E:\Dep-Gandia\Prensa  /remove:g Usuarios
 icacls E:\Dep-Gandia\Informatica  /remove:g Usuarios
 icacls E:\Dep-Gandia\Investigacion_desarollo /remove:g Usuarios
-REM
+REM  //añandimos los grupos y permisos de seguridad
 icacls E:\Dep-Gandia\RRHH /GRANT gandia\Gandia_GL_Lectura:(R)
 icacls E:\Dep-Gandia\Prensa /GRANT gandia\Gandia_GL_Lectura:(R)
 icacls E:\Dep-Gandia\Informatica /GRANT gandia\Gandia_GL_Lectura:(R)
